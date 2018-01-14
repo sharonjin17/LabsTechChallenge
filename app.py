c03a1da33159e9774989f8eec85ea280310cdeb9
sharonjin17 <sharonjin17@gsbschool.org>
2018-01-14 14:16:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/sj2846')
+def sj():
+    return '<h1>Sharon Jin</h2><img src="http://cdn.kickvick.com/wp-content/uploads/2015/09/cutest-bunny-rabbits-10.jpg" alt="Cute sleepy bunny :)" style="width:142px;height:142px;"><style>body{background-color:#e085dd}</style>' 
+
 
 if __name__ == '__main__':
     app.run()
