7794a1e19c4cf413974d9a1f3b388a5f8f6cbcd1
sharonjin17 <sharonjin17@gsbschool.org>
2018-01-14 15:47:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ def main():
 
 @app.route('/sj2846')
 def sj():
-    return '<h1>Sharon Jin</h2><img src="http://cdn.kickvick.com/wp-content/uploads/2015/09/cutest-bunny-rabbits-10.jpg" alt="Cute sleepy bunny :)" style="width:142px;height:142px;"><style>body{background-color:#e085dd}</style>' 
+    return '<h1>Sharon Jin</h2> \
+<img src="http://cdn.kickvick.com/wp-content/uploads/2015/09/cutest-bunny-rabbits-10.jpg" \
+alt="Cute sleepy bunny :)" style="width:142px;height:142px;"> \
+<style>body{background-color:#e085dd}</style> \
+<h2>My favorite color is light purple, like the background :)</h2>'
 
 
 if __name__ == '__main__':
